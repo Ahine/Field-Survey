79015dd68dbb31a214dc0bb975857e1cd41d3f0d
Andy Hine <andyhine@Andys-MacBook-Pro.local>
2017-10-28 12:13:28 -0700

    initial project commit

---
+++ Field Survey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Field Survey
+//
+//  Created by Andy Hine on 10/28/17.
+//  Copyright © 2017 Andy Hine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
